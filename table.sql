51aeafc5650c0184a8b94e74ba25501c7aac19ad
Ruohao Wu <wu_r1@denison.edu>
2022-11-07 20:29:35 -0500

    Create initial three table about hospital, hospital info, and hospital quality

---
+++ table.sql
@@ -0,0 +1,39 @@
+--Create a table named hospital
+create table hospital(
+    hospital_pk text PRIMARY KEY,
+    hospital_name text NOT NULL,
+    city text,
+    county text,
+    state varchar(2),
+    address text,
+    zip text,
+    fips text,
+    lautitude float,
+    longitude float,
+    ownership text,
+    emergency_availability boolean default NULL);
+
+-- Create a table named hospital_info
+create table hospital_info(
+    ID SERIAL PRIMARY KEY,
+    hospital_pk text,
+    date DATE check(date<CURRENT_DATE),
+    adult_bed_available float,
+    child_bed_available float,
+    adult_bed_occupied float,
+    child_bed_occupied float,
+    ICU_bed_available float,
+    ICU_bed_occupied float,
+    COVID_patient float,
+    ICU_COVID_patient float,
+    FOREIGN KEY(hospital_pk)
+    REFERENCES hospital(hospital_pk) MATCH FULL);
+
+-- Create a table named quality
+create table hospital_quality(
+    ID SERIAL PRIMARY KEY,
+    hospital_pk stirng,
+    quality numeric,
+    date DATE,
+    FOREIGN KEY(hospital_pk)
+    REFERENCES hospital(hospital_pk) MATCH FULL);
